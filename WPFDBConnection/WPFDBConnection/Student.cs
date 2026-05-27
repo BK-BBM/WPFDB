5af3fb5a176b9d40f6819cf2c226c2ad71f9fdad
BK-BBM <33920592+BK-BBM@users.noreply.github.com>
2026-05-27 16:19:35 +0200

    Update files -- 16H20

--- WPFDBConnection/WPFDBConnection/Student.cs
+++ WPFDBConnection/WPFDBConnection/Student.cs
@@ -1,8 +1,129 @@
-﻿namespace WPFDBConnection
+﻿using MySql.Data.MySqlClient;
+
+namespace WPFDBConnection
 {
     public class StudentCRUD
     {
         //this is our controller -> get data from DB (model) , display it on the student.xaml(view)
+
+        /*
+         In the DB-> Select * From Students 
+         */
+        public List<Student> GetAll(string q = "")
+        {
+            var list = new List<Student>();
+
+            using var conn = DB.Open();
+
+            string sql = """
+
+                SELECT * From students 
+                Where @q=''
+                OR student_no LIKE @like
+                OR first_name LIKE @like
+                OR last_name LIKE @like
+                OR email LIKE @like
+                OR status LIKE @like
+                """;
+
+            //this block prepares query to be run in SQL
+            using var command = new MySqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@q", q);
+            command.Parameters.AddWithValue("@like", $"%{q}%");
+
+            //Executes query 
+            using var reader = command.ExecuteReader();
+
+            //displays (maps it to the correct property) the data as the "cursor" object is going through 
+            //the DB
+            while (reader.Read())
+            {
+                list.Add(Map(reader));
+            }
+
+            // map sql columns to model properties 
+            return list;
+        } // Select * From Students Where first_name LIKE = %'value'%
+
+        /* INSERT, UPDATE & DELETE Methods
+         */
+
+        public void Insert(Student s) {
+            //Insert Into Student (columnName) Values ('Value');
+
+            //create a connection to DB
+            using var conn = DB.Open();
+            string sql = """
+                INSERT Into students (student_no,first_name, last_name,
+                email, phone, dateofbirth, status)
+                Values(@no, @fn, @ln,@email,@phone,@dob,@status)
+                """;
+            using var command = new MySqlCommand(sql, conn);
+
+            Bind(command, s);
+            command.ExecuteNonQuery();
+            
+            //because id is AUTO INCREMENTING (we get it from the DB) 
+            //LastInsertedId will return the "biggest" value
+            //biggest value = last inserted id because we increment with 1.
+            s.Id = (int)command.LastInsertedId;
+        }
+
+        public void Update(Student s) {
+
+            using var conn = DB.Open();
+            string sql = """
+                UPDATE students SET 
+                student_no = @no, first_name = @fn, last_name = @ln,
+                email = @email, phone = @phone, dateofbirth = @dob, 
+                status = @status
+                WHERE id = @id
+                """;
+
+            using var command = new MySqlCommand(sql, conn);
+
+            Bind(command, s);
+            command.Parameters.AddWithValue("@id", s.Id);
+            command.ExecuteNonQuery();
+        }
+
+        public void Delete(int id) { 
+            using var conn = DB.Open();
+            using var command = new 
+                MySqlCommand("DELETE FROM students WHERE id=@id", conn);
+
+            command.Parameters.AddWithValue("@id", id);
+            command.ExecuteNonQuery();
+        }
+
+
+        //Map and Bind Methods
+        private static Student Map(MySqlDataReader reader) => new()
+        {
+            Id = reader.GetInt32("id"),
+            StudentNo = reader.GetString("student_no"),
+            FirstName = reader.GetString("first_name"),
+            LastName = reader.GetString("last_name"),
+            Email = reader.GetString("email"),
+            Phone = reader.IsDBNull(reader.GetOrdinal("phone")) ?"" 
+            : reader.GetString("phone"),
+            DateOfBirth = reader.IsDBNull(reader.GetOrdinal("dateofbirth"))? null
+            : reader.GetDateTime("dateofbirth"),
+            Status = reader.GetString("status"),
+        };
+
+
+        private static void Bind(MySqlCommand command, Student student)
+        {
+            command.Parameters.AddWithValue("@no", student.StudentNo);
+            command.Parameters.AddWithValue("@fn", student.FirstName);
+            command.Parameters.AddWithValue("@ln", student.LastName);
+            command.Parameters.AddWithValue("@email", student.Email);
+            command.Parameters.AddWithValue("@phone", student.Phone);
+            command.Parameters.AddWithValue("@dob", (object?)student.DateOfBirth ?? 
+                DBNull.Value);
+            command.Parameters.AddWithValue("@status", student.Status);
+        }
 
     }
 }
--- WPFDBConnection/WPFDBConnection/Student.xaml.cs
+++ WPFDBConnection/WPFDBConnection/Student.xaml.cs
@@ -18,51 +18,148 @@ namespace WPFDBConnection
     /// </summary>
     public partial class Page1 : Page
     {
-       
+        private readonly StudentCRUD studentCRUD = new();
+        private Student? student;
         public Page1()
         {
             InitializeComponent();
+            Load();
             
+        }
+
+        private void Load(string q = "")
+        {
+            try
+            {
+                var list = studentCRUD.GetAll(q);
+                Grid.ItemsSource = list;
+                //TxtCount.Text = 
+            }
+            catch (Exception ex) 
+            {
+                Error(ex);
+            }
         }
 
       
 
         private void TxtSearch_Changed(object sender, TextChangedEventArgs e)
         {
-            
+            Load(TxtSearch.Text.Trim());
         }
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
-            
+            student = null;
             ClearForm();
+            TxtNo.Focus();
             
         }
 
         private void Grid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-         
+            if (Grid.SelectedItem is not Student student_) return;
+
+            student = student_;
+            TxtNo.Text = student_.StudentNo;
+            TxtFirst.Text = student_.FirstName;
+            TxtLast.Text = student_.LastName;
+            TxtEmail.Text = student_.Email;
+            TxtPhone.Text = student_.Phone;
+            TxtDob.Text = student_.DateOfBirth?.ToString("yyyy-MM-dd")?? "";
+            TxtNo.Text = student_.StudentNo;
+            SetCombo(CmbStatus, student_.Status);
         }
 
         private void BtnClear_Click(object sender, RoutedEventArgs e)
         {
-            
+            student = null;
             ClearForm();
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (student is null)
+            {
+                MessageBox.Show("Select a student first", "Delete",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var notification = MessageBox.Show(
+                $"Delete {student.FirstName}?", "Confirm Deletion",
+                MessageBoxButton.YesNo, MessageBoxImage.Warning
+                );
+
+            if (notification == MessageBoxResult.Yes) {
+                try
+                {
+                    studentCRUD.Delete(student.Id);
+                    ClearForm();
+                    Load();
+                }
+                catch (Exception ex)
+                {
+                    Error(ex);
+                }
+
+            }
         }
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
-            
+            //if(string.IsNullOrWhiteSpace(TxtNo.Text))
+
+            DateTime? dob = null;
+            if (!string.IsNullOrEmpty(TxtDob.Text))
+            {
+                if (!DateTime.TryParse(TxtDob.Text, out var date)) {
+                    MessageBox.Show("Format date as yyyy-MM-dd");
+                    return;
+                }
+                dob = date;
+            }
+            try
+            {
+                var stu = new Student
+                {
+                    Id = student?.Id ?? 0,
+                    StudentNo = TxtNo.Text,
+                    FirstName = TxtFirst.Text,
+                    LastName = TxtLast.Text,
+                    Email = TxtEmail.Text,
+                    Phone = TxtPhone.Text,
+                    DateOfBirth = dob,
+                    Status = (CmbStatus.SelectedItem as
+                    ComboBoxItem)?.Content?.ToString()?? "Active",
+                };
+
+                if (student is null)
+                {
+                    studentCRUD.Insert(stu);
+                }
+                else
+                {
+                    studentCRUD.Update(stu);
+                }
+                ClearForm();
+                Load();
+            }
+            catch(Exception ex)
+            {
+                Error(ex);
+            } 
+
         }
 
         private void ClearForm()
         {
-            
+            student = null;
+            TxtFormTitle.Text = "Add Student";
+            TxtNo.Text = TxtFirst.Text = TxtLast.Text
+                = TxtEmail.Text = TxtPhone.Text = TxtDob.Text = "";
+            CmbStatus.SelectedIndex = 0;
+            Grid.SelectedItem = null;
         }
         private static void SetCombo(ComboBox cmb, string value)
         {
